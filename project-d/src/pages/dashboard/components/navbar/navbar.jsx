6a820751d87e0b3dae7b7855b9b3518ac899243f
Minyoui <evanlyrush73@gmail.com>
2025-08-14 00:31:34 +0800

    5th Commit (Navbar with Mobile Responsive)

--- project-d/src/pages/dashboard/components/navbar/navbar.jsx
+++ project-d/src/pages/dashboard/components/navbar/navbar.jsx
@@ -1,9 +1,61 @@
 import './navbar.css';
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+//icons
+import dropdown from '../../../../assets/icons/dropdown.png';
+import circleArrow from '../../../../assets/icons/circle-up-right.png';
+//other images
+import TextLogo from '../../../../assets/business/maison-d-text-logo.svg';
 
 function Navbar () {
+    const [scrolled, setScrolled] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    // Navbar scroll identifier
+    useEffect(() => {
+        const handleScroll = () => {
+            const offset = window.scrollY;
+            setScrolled(offset > 80);
+        };
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    // Mobile Nav screen automatically closes when resized to desktop
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth > 720) {
+                setMenuOpen(false);
+            }
+        };
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+
     return (
         <>
-            <h1>HELLO WORLD</h1>
+            <div className='nav-wrapper'>
+                <nav className={`nav-container ${scrolled? 'scrolled' : ''}`}
+                     id={`${menuOpen? 'nav-active' : ''}`}>
+                    <a href="/" className='text-logo'><img src={TextLogo}/></a>
+                    <ul className={`nav-links ${menuOpen? 'nav-active' : ''}`}>
+                        <li><a href='#home'>HOME<div className='nav-underline'/></a></li>
+                        <li><a href='#home' id='products-menu'>PRODUCTS<img src={dropdown}/></a></li>
+                        <li><a href='#home'>FAQs</a></li>
+                        <li><a href='#home'>CONTACT US</a></li>
+                    </ul>
+                    <Link to='/' className='sign-in-link'>Sign Up / Sign In<img src={circleArrow}/></Link>
+                    
+                    <div className={`hamburger ${menuOpen? 'nav-active' : ''}`} onClick={() => setMenuOpen(!menuOpen)}>
+                        ☰
+                    </div>
+                    <div className={`cancel ${menuOpen? 'nav-active' : ''}`} onClick={() => setMenuOpen(!menuOpen)}>
+                        X
+                    </div>
+                </nav>
+            </div>
         </>
     )
 }
