bf0f55a8579f3f121f78109ce4c27b1f2fd875be
Minyoui <evanlyrush73@gmail.com>
2025-08-13 23:01:21 +0800

    4th Commit (Navbar Preparation)

--- project-d/src/App.jsx
+++ project-d/src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
-import './App.css' 
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import Navbar from '../src/pages/dashboard/components/navbar/navbar.jsx';
 
-const Home = () => {
-
-};
+const Home = () => (
+  <>
+    <Navbar />
+  </>
+);
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home />}/>
+        <Route path="/" element={<Home />}/>
       </Routes>
     </>
   );
---
+++ project-d/src/pages/dashboard/components/navbar/navbar.jsx
@@ -0,0 +1,11 @@
+import './navbar.css';
+
+function Navbar () {
+    return (
+        <>
+            <h1>HELLO WORLD</h1>
+        </>
+    )
+}
+
+export default Navbar;
