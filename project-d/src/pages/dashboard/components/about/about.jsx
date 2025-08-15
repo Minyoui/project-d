6b482600c5dde24f05ce12d8cba2809a65f96f00
Minyoui <evanlyrush73@gmail.com>
2025-08-15 18:16:56 +0800

    11th Commit (About Section)

--- project-d/src/pages/dashboard/components/about/about.jsx
+++ project-d/src/pages/dashboard/components/about/about.jsx
@@ -1,11 +1,45 @@
 import './about.css';
-import BlurText from '../../context/react-bits/BlurText/BlurText';
+import { useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+// other imgs
+import  Leaves from '../../../../assets/business/MD-leaves.png';
+import AboutImg from '../../../../assets/business/MD-about-img.png';
 
 function About () {
+    const sectionRef = useRef(null);
+    const [inView, setInView] = useState(false);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                setInView(entry.isIntersecting);
+            },
+            { threshold: 0.3 } // 30% of element is visible
+        );
+
+        if (sectionRef.current) {
+            observer.observe(sectionRef.current);
+        }
+
+        return () => {
+            if (sectionRef.current) {
+                observer.unobserve(sectionRef.current);
+            }
+        };
+    }, []);
+
     return (
         <>
             <div className='about-wrapper'>
-
+                <div ref={sectionRef} className={`about-container ${inView? 'animate' : ''}`}>
+                    <h1>Experience Calm, Curated with Luxurious Care</h1>
+                    <div className='abt-line'/>
+                    <p>Maison`D is a sanctuary of relaxation, offering authentic balinese spa treatments designed to heal and rejuvenate</p>   
+                </div>
+                <Link to="/" ref={sectionRef} className={`about-link ${inView? 'animate' : ''}`}>
+                    <img src={AboutImg} className='sample-photo' alt='Sample Photo'/>
+                </Link>
             </div>
         </>
     )
