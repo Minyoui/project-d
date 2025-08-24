3b64f73e711557e185e53ab229ccd9ac9e816b3e
Minyoui <evanlyrush73@gmail.com>
2025-08-24 15:29:28 +0800

    25th Commit (AboutPage: Hero Update)

--- project-d/src/pages/about/components/about-hero/about-hero.jsx
+++ project-d/src/pages/about/components/about-hero/about-hero.jsx
@@ -1,10 +1,17 @@
 import './about-hero.css';
 
+// Business
+import Logo from '../../../../assets/business/MD-Logo.svg';
+import Team from '../../../../assets/business/maison-team.png';
+
 function AboutHero () {
     return (
         <>
             <div className='about-hero-section'>
-
+                <img src={Logo} alt="Maison`D" />
+                <p>
+                    OUR STORY BEGINS WITH YOUR SERENITY
+                </p>
             </div>
         </>
     )
